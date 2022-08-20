0a3338075d81b5a4b92cb6e0bb0aff2f601dfb91
lukejrichardson <rchluk003@myuct.ac.za>
2022-08-20 14:27:33 +0200

    Part b

--- Prac2.c
+++ Prac2.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 
+#define TITLE "DECIMAL TO RADIX-i converter"
+#define AUTHOR "Luke Richardson"
+#define YEAR 2022
+
 int main(){
     
 	printf("*****************************\n");
-	printf("DECIMAL TO RADIX-i converter\n");
-	printf("Written by: Luke Richardsob\n");
-	printf("Date: 2022\n");
+	printf("%s\n", TITLE);
+	printf("Written by: %s\n", AUTHOR);
+	printf("Date: %d\n", YEAR);
 	printf("*****************************\n");
 	
     return 0;
