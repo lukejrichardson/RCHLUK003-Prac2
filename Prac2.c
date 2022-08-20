09a638b1559452e16baf5be64035996d4d051146
lukejrichardson <rchluk003@myuct.ac.za>
2022-08-20 14:20:36 +0200

    Part a

--- Prac2.c
+++ Prac2.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
 int main(){
+    
+	printf("*****************************\n");
+	printf("DECIMAL TO RADIX-i converter\n");
+	printf("Written by: Luke Richardsob\n");
+	printf("Date: 2022\n");
+	printf("*****************************\n");
+	
     return 0;
 }
