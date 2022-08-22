72078a41f5d98a816c2c095eda6dca667d45ca01
lukejrichardson <rchluk003@myuct.ac.za>
2022-08-22 16:24:18 +0200

    Part f

--- Prac2.c
+++ Prac2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 
 #define TITLE "DECIMAL TO RADIX-i converter"
 #define AUTHOR "Luke Richardson"
@@ -16,17 +17,20 @@ int main(){
 	int rad;
 	
 	while(1){
-
+		
 		printf("Enter a decimal value: ");
 		scanf("%d", &dec);
+		
 		if (dec<=0){
 			printf("EXIT");
 			break;
 		}
+		
 		printf("The number you have entered is %d\n", dec);
 		printf("Enter a radix for the converter between 2 and 16: ");
 		scanf("%d", &rad);
 		printf("The radix you have entered is %d\n", rad);
+		printf("The log2 of the number is %.2f\n", log2f(dec));
 		
 	}
 
