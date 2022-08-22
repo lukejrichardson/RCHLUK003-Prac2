b8126ec352037b0296b5ba966b199bb1b06f8032
lukejrichardson <rchluk003@myuct.ac.za>
2022-08-22 16:18:24 +0200

    Part e

--- Prac2.c
+++ Prac2.c
@@ -13,16 +13,23 @@ int main(){
 	printf("*****************************\n");
 	
 	int dec;
-	
-	printf("Enter a decimal value: ");
-	scanf("%d", &dec);
-	printf("The number you have entered is %d\n", dec);
-	
 	int rad;
 	
-	printf("Enter a radix for the converter between 2 and 16: ");
-	scanf("%d", &rad);
-	printf("The radix you have entered is %d\n", rad);
+	while(1){
+
+		printf("Enter a decimal value: ");
+		scanf("%d", &dec);
+		if (dec<=0){
+			printf("EXIT");
+			break;
+		}
+		printf("The number you have entered is %d\n", dec);
+		printf("Enter a radix for the converter between 2 and 16: ");
+		scanf("%d", &rad);
+		printf("The radix you have entered is %d\n", rad);
+		
+	}
+
 	
     return 0;
 }
